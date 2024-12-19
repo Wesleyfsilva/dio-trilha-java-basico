febf49fa106003764c1f6e1a8c8b7d7118e7402d
Wesley Ferreira <wesleyfsilva97@gmail.com>
2024-12-19 01:44:47 -0300

    Meu projeto conta banco

---
+++ Comentarios/src/Comentarios.java
@@ -0,0 +1,11 @@
+public class Comentarios {
+  public static void main(String[] args){
+    /*Comentario de uma linha */
+
+    /*
+     * Esse comentario foi elaborado as pressas para testar comentarios
+     * Esse tipo é ultilizado em multiplas linhas
+     * Como nesse exemplo
+     */
+  }
+}
